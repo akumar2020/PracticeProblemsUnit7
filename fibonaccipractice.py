166750bb2290c50273c65b615f2eec883d2754b2
Ashna Kumar <akumar2020@pingry.org>
2017-04-27 09:04:19 -0400

    I finshed my fibonacci function and created multiple test cases.

---
+++ fibonaccipractice.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+	'''Takes an index 'n' and returns the nth fibonacci number'''
+	if n <= 1:
+		return 1
+	else:
+		return (fibonacci(n - 2) + fibonacci(n - 1))
+
+print(fibonacci(0))
+print(fibonacci(1))
+print(fibonacci(40))
+print(fibonacci(9))
